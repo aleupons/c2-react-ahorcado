d937e838a441a612fcfdea89b0f29390e468b3e7
Pol Reig <polreigbroto@gmail.com>
2021-06-20 17:57:39 +0200

    Recojer y pintar letras falladas

--- src/components/LetrasUsadas.js
+++ src/components/LetrasUsadas.js
@@ -1,3 +1,19 @@
-export const LetrasUsadas = () => {
-  return <ul className="letras-usadas"></ul>;
+export const LetrasUsadas = (props) => {
+  const { letrasFalladas } = props;
+  return (
+    <>
+      <ul className="letras-usadas">
+        {letrasFalladas
+          .map((letra, i) => (
+            <li className="letras-usadas" key={i}>
+              {letra.toUpperCase()}
+            </li>
+          ))
+          .reduce(
+            (prev, curr) => (prev === null ? [curr] : [prev, " - ", curr]),
+            null
+          )}
+      </ul>
+    </>
+  );
 };
